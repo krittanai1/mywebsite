fb4cbc4cef0892a9eccb9e53412483f79a04cbe8
krittanai1 <98872017+krittanai1@users.noreply.github.com>
2022-02-23 14:29:04 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -3,7 +3,7 @@
     <title>งานเรื่อง link</title>
   </head>
   <body>
-    <a href="https://facebook.com"<h1>จิ้มจิ้ม</h1></a>
+    <a href="https://facebook.com"<h1>จิ้ม</h1></a>
     <a href="mypage2.php"><h1>page 2</h1></a>
   </body>
 </html>
