53b9ca021b8b23995514317af733b8ef25d2c87c
krittanai1 <98872017+krittanai1@users.noreply.github.com>
2022-02-23 14:34:42 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("MiPancakeBold.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("sd chokdee.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">กิตติพศ พั้วพวง ม.4/12 เลขที่ 3 เลขประจำตัว 57012</h1>
+    <h1 id="font2">กิตติพศ พั้วพวง ม.4/12 เลขที่ 3 เลขประจำตัว 57012</h1>
+  </body>
+</html>
