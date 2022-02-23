5b38d6b62093faf8489464301496a2db1e180821
krittanai1 <98872017+krittanai1@users.noreply.github.com>
2022-02-23 14:28:46 +0700

    Create mypage1.php

---
+++ mypage1.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>งานเรื่อง link</title>
+  </head>
+  <body>
+    <a href="https://facebook.com"<h1>จิ้มจิ้ม</h1></a>
+    <a href="mypage2.php"><h1>page 2</h1></a>
+  </body>
+</html>
