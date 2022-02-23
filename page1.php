539ac6f83063398dc05e4620d82b81bed8a35fbf
krittanai1 <98872017+krittanai1@users.noreply.github.com>
2022-02-23 14:25:10 +0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,20 @@
+<html>
+  <head> 
+    <title>page 1</title> 
+  </head>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br> 
+    <b>ชื่อ :</b> <i>นาย กฤตนัย สามลทา</i>
+    <br> 
+    <b>ชื่อเล่น :</b> <i>ปั๊บ</i> 
+    <br>
+    <b>ชั้น :</b> <i>ม.4/12</i>
+    <br> 
+    <b>เลขที่ :</b> <i>1</i>
+    <br>
+    <b>เลขประจำตัวนักเรียน :</b> <i>57009</i>
+    <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i> 
+    </body>
+    </html>
