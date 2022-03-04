d1096568489857bb54f01d7ac0cf0922b12c749b
krittanai1 <98872017+krittanai1@users.noreply.github.com>
2022-03-05 02:16:27 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>IG/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>marshmello</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Superbus.png" width="45px"></td>
+        <td><a href="https://www.instagram.com/marshmellomusic/">instagram</a></td>
+         <tr>
+        <td>2</td>
+        <td>Lil Bibby</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/5/59/Lil_Bibby.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/lilbibby_/">instagram</a></td>
+            <tr>
+        <td>3</td>
+        <td>youngohmm</td>
+        <td><img src="https://cheezelooker.com/file_managers/uploads/file_managers/source/STREET/0022_19-03-63/Beer/untitled%20folder%2002/StreetBangkok_CheezeLooker_beer-15.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/youngohmm/">instagram</a></td>
+               <tr>
+        <td>4</td>
+        <td>Cardi B</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Cardi_B_-_Openair_Frauenfeld_2019_02_%28cropped%29.jpg" width="45px"></td>
+        <td><a href="https://www.instagram.com/iamcardib/">instagram</a></td>
+                  <tr>
+        <td>5</td>
+        <td>Maiyarap</td>
+        <td><img src="https://www.naewna.com/uploads/news/source/618157.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/realmaiyarap">facebook</a></td>
+                     <tr>
+      </tr>
+    </table>
+  </body>
+</html>
